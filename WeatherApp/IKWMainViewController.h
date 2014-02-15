8c8b05c3b9c23bf92110ecadda677fb57f4e09cd
ilteris <ilteris@gmail.com>
2014-02-15 13:06:49 -0500

    add background and app structure

---
+++ WeatherApp/IKWMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  IKWMainViewController.h
+//  WeatherApp
+//
+//  Created by ilteris on 2/15/14.
+//  Copyright (c) 2014 ilteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKWMainViewController : UIViewController
+
+@end
---
+++ WeatherApp/IKWMainViewController.m
@@ -0,0 +1,39 @@
+//
+//  IKWMainViewController.m
+//  WeatherApp
+//
+//  Created by ilteris on 2/15/14.
+//  Copyright (c) 2014 ilteris. All rights reserved.
+//
+
+#import "IKWMainViewController.h"
+
+@interface IKWMainViewController ()
+
+@end
+
+@implementation IKWMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"ViewDidLoad");
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
