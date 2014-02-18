5feb8a16e438973fe834fe782bdd04fe4b1cff5e
ilteris <ilteris@gmail.com>
2014-02-18 00:06:48 -0500

    add scrollview

--- WeatherApp/IKWMainViewController.m
+++ WeatherApp/IKWMainViewController.m
@@ -16,6 +16,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *currentDegreesLabel;
 @property (strong, nonatomic) IBOutlet ReflectionView *reflectionView;
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *view1;
+@property (weak, nonatomic) IBOutlet UIView *view2;
 
 @end
 
@@ -59,6 +62,9 @@
 {
     [super viewDidLoad];
 
+    self.scrollView.contentSize = CGSizeMake(2 * self.view.frame.size.width, self.view.frame.size.height);
+
+    
     [self.currentDegreesLabel setFont:[UIFont fontWithName:@"Gotham-Book" size:110]];
     [self.locationNameLabel setFont:[UIFont fontWithName:@"Gotham-Medium" size:11]];
     [self.currentWeatherLabel setFont:[UIFont fontWithName:@"Gotham-Book" size:17]];
