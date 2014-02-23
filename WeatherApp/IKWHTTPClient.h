9c546c2c31c390fdc37527924e1432f61a78e047
ilteris <ilteris@gmail.com>
2014-02-23 18:20:29 -0500

    add forecast.io client file

---
+++ WeatherApp/IKWHTTPClient.h
@@ -0,0 +1,13 @@
+//
+//  IKWHTTPClient.h
+//  
+//
+//  Created by ilteris on 2/23/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKWHTTPClient : AFHTTPClient
+
+@end
---
+++ WeatherApp/IKWHTTPClient.m
@@ -0,0 +1,13 @@
+//
+//  IKWHTTPClient.m
+//  
+//
+//  Created by ilteris on 2/23/14.
+//
+//
+
+#import "IKWHTTPClient.h"
+
+@implementation IKWHTTPClient
+
+@end
