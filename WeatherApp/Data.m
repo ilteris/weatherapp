ec43e80305b33591d8cd5228180c19fa76dab866
ilteris <ilteris@gmail.com>
2014-03-15 15:09:15 -0400

    Add new models

---
+++ WeatherApp/Data.m
@@ -0,0 +1,33 @@
+//
+//  Data.m
+//  WeatherApp
+//
+//  Created by ilteris on 3/15/14.
+//  Copyright (c) 2014 ilteris. All rights reserved.
+//
+
+#import "Data.h"
+#import "Location.h"
+
+
+@implementation Data
+
+@dynamic apparentTemperature;
+@dynamic cloudCover;
+@dynamic dewPoint;
+@dynamic humidity;
+@dynamic icon;
+@dynamic ozone;
+@dynamic precipIntensity;
+@dynamic precipProbability;
+@dynamic pressure;
+@dynamic summary;
+@dynamic temperature;
+@dynamic time;
+@dynamic windBearing;
+@dynamic windSpeed;
+@dynamic timeFrame;
+@dynamic createdAt;
+@dynamic location;
+
+@end
