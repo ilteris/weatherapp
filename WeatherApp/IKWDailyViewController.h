5bd7a7be7c289619aeea9d1060b27eb7af4547d5
ilteris <ilteris@gmail.com>
2014-04-10 11:08:26 -0400

    add tableview for daily second screen

---
+++ WeatherApp/IKWDailyViewController.h
@@ -0,0 +1,13 @@
+//
+//  IKWDailyViewController.h
+//  WeatherApp
+//
+//  Created by ilteris on 4/10/14.
+//  Copyright (c) 2014 ilteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKWDailyViewController : UIViewController
+
+@end
