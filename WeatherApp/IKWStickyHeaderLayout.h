a7920fc34da8e34f9a4ae92f2570704e5c4c1db3
ilteris <ilteris@gmail.com>
2014-04-12 21:37:14 -0400

    custom flowlayout to make the header sticky

---
+++ WeatherApp/IKWStickyHeaderLayout.h
@@ -0,0 +1,13 @@
+//
+//  IKWStickyHeaderLayout.h
+//  WeatherApp
+//
+//  Created by ilteris on 4/12/14.
+//  Copyright (c) 2014 ilteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKWStickyHeaderLayout : UICollectionViewFlowLayout
+
+@end
